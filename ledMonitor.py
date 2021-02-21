fe1dc5b3002634826dbb094f3ae439ec9b893e3c
ZengjfOS <zengjf42@163.com>
2021-02-21 05:18:32 +0000

    led contorl

---
+++ ledMonitor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, Response, request
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        # 通过opencv获取实时视频流
+        self.video = cv2.VideoCapture(0) 
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+app = Flask(__name__)
+
+@app.route('/')  # 主页
+def index():
+    # jinja2模板，具体格式保存在index.html文件中
+    return render_template('led.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')   
+
+@app.route('/led')
+def led_change():
+	ledStatus = request.args.get('setValue', default = '*', type = str)
+	print(ledStatus)
+
+	with open("/sys/class/gpio/gpio10/value", 'w+') as out:
+		if (ledStatus == "Led On"):
+			out.write('0\n')
+		else:
+			out.write('1\n')
+
+	return ""
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)  
+
